8808ff3eeff8c1e136911b83609619033bac2455
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-02-09 21:33:27 +0530

    Added password for clearing database

--- Connection/Frontend/src/assets/components/Delete/Delete.jsx
+++ Connection/Frontend/src/assets/components/Delete/Delete.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const Delete = () => {
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [accessGranted, setAccessGranted] = useState(false);
+
     const clearDatabase = () => {
-        console.log('Clearing database...'); // Check if this message appears in the console
         axios.post('http://localhost:8000/api/clear-database/')
             .then(response => {
                 console.log(response.data); // Database cleared successfully message
@@ -12,13 +15,43 @@ const Delete = () => {
                 console.error('Error clearing database:', error);
             });
     };
-    
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (password === '1234') {
+            setAccessGranted(true);
+        } else {
+            setError('Wrong password');
+        }
+    };
 
     return (
-        <div>
-            <button onClick={clearDatabase}>Clear Database</button>
+        <div className='flex justify-center mt-10 items-center border border-red-500 w-96 mx-auto'>
+            {!accessGranted && (
+                <form onSubmit={handleSubmit}>
+                    <input
+                        className='bg-black w-96 h-10 text-center text-white'
+                        type="password"
+                        placeholder="Enter admin password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button
+                        type="submit"
+                        className="flex flex-col mt-3 mx-auto bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                    >
+                        Submit
+                    </button>
+                    {error && <p>{error}</p>}
+                </form>
+            )}
+            {accessGranted && (
+                <div>
+                    <button onClick={clearDatabase}>Clear Database</button>
+                </div>
+            )}
         </div>
     );
-}
+};
 
 export default Delete;
