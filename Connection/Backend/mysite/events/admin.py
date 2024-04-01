f917a669e183b41624e87b64e0b35ce0ef57bb53
Rehman Khan <rehman.khan.300a@gmail.com>
2024-04-02 00:54:12 +0530

    removed customusermodel

--- Connection/Backend/mysite/events/admin.py
+++ Connection/Backend/mysite/events/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import CustomUserModel
+from .models import Event, SubEvent, RegisteredEvent
 
-class CustomUserModelAdmin(admin.ModelAdmin):
-    list_display = ('email',)  # fields to display in list view
-    search_fields = [ 'email']  # fields to search in the admin interface
+admin.site.register(Event)
+admin.site.register(SubEvent)
+admin.site.register(RegisteredEvent)
 
-admin.site.register(CustomUserModel, CustomUserModelAdmin)
+
+
--- Connection/Backend/mysite/events/models.py
+++ Connection/Backend/mysite/events/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from datetime import time
 from django.utils import timezone
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class Event(models.Model): 
     name = models.CharField(max_length=255)
@@ -29,41 +28,6 @@ class SubEvent(models.Model):
 
 
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username=None, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        if not username:
-            username = 'default_username'
-        email = self.normalize_email(email)
-        user = self.model(email=email, username=username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, username=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        if not username:
-            username = 'default_superuser'
-        return self.create_user(email, username, password, **extra_fields)
-
-
-class CustomUserModel(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=150, unique=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(auto_now_add=True)
-
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = 'email'
-    EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    def __str__(self):
-        return self.email
 class RegisteredEvent(models.Model):
     username = models.CharField(max_length=150)
     sub_event_name = models.CharField(max_length=255)  # New field for sub-event name
