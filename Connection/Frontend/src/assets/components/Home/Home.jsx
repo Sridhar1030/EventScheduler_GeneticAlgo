86d3d0b679ebff652cb47471177c1574e798c44f
Sridhar Pillai <77533416+Sridhar1030@users.noreply.github.com>
2024-11-20 03:34:02 +0530

    ,

--- Connection/Frontend/src/assets/components/Home/Home.jsx
+++ Connection/Frontend/src/assets/components/Home/Home.jsx
@@ -1,73 +1,50 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Navbar from '../Navbar/Navbar';
-import gif from '../../Gif/Final.gif'
-import svg from '../../Gif/SVG.svg'
+import gif from '../../Gif/Final.gif';
 import { Link } from 'react-router-dom';
-import AuthContext from '../../../Context/AuthContext';
-const Home = () => {
 
+const Home = () => {
     return (
-        <div className="flex flex-col min-h-screen bg-[#0D1117] text-white ">
+        <div className="flex flex-col min-h-screen bg-[#0D1117] text-white">
             <div className="flex-grow">
                 <Navbar />
-
-                <div className="container mx-auto py-20 ">
+                <div className="container mx-auto py-20">
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-10 ml-4">
-                        <div className="flex 3
-                        flex-col justify-center">
+                        <div className="flex flex-col justify-center">
                             <h1 className="text-4xl md:text-5xl font-bold mb-4">
-                                Welcome to <div className='font-serif p-1'>RoundGen</div> a scheduling website 
-                                {/* {user.username} {user.is_superuser ? 'Admin' : ''} */}
+                                Welcome to <div className="font-serif p-1">RoundGen</div> a scheduling website
                             </h1>
                             <p className="text-lg">
                                 Event Scheduler
                                 Post your events and sub-events and schedule them using the
-                                Genetic Algorithm and get match schedule using modified round robin algorithm
+                                Genetic Algorithm and get match schedule using the modified round-robin algorithm.
                             </p>
-                            <div className='mt-10 ml-10 gap-10 mx-auto '>
-                                {user && user.is_superuser ?
-                                    <>
-                                        <h1 className='text-xl'>Create a Event </h1>
-                                        <Link to="/create">
-                                            <button className="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-white">
-                                                Create
-                                            </button>
-                                        </Link>
-                                    </>
-                                    : <></>}
-                                <h1 className=' mt-10 text-xl'>See you events </h1>
+                            <div className="mt-10 ml-10 gap-10 mx-auto">
+                                <h1 className="mt-10 text-xl">See your events</h1>
                                 <Link to="/enrolled">
                                     <button className="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-white">
                                         Your events
                                     </button>
                                 </Link>
                             </div>
-
-
                         </div>
                         <div>
                             <img
                                 src={gif}
                                 alt="Homepage"
-
-                                className="size-[450px] right-20 absolute "
+                                className="size-[450px] right-20 absolute"
                             />
-                            <div>
-
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
             <div className="bg-black py-10 text-center">
-                <div className='mb-1'>
+                <div className="mb-1">
                     <p className="text-lg">Contact us for more information</p>
-                    <p className="text-2xl font-bold"></p>
                 </div>
             </div>
         </div>
     );
 };
 
-export default Home;    
+export default Home;
