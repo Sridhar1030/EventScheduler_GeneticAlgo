8b9fdc29b6f3ecd970324e48e9b992096334a21c
Sridhar Pillai <77533416+Sridhar1030@users.noreply.github.com>
2024-11-20 03:30:55 +0530

    ,,

--- Connection/Frontend/src/App.jsx
+++ Connection/Frontend/src/App.jsx
@@ -10,23 +10,21 @@ import YourEvents from './assets/components/YourEvents/YourEvents';
 import Login from './assets/components/Registration/Login';
 import Scheduler from './assets/components/Scheduler/Scheduler';
 import Genetic from './assets/components/Genetic/Genetic';
-import { AuthProvider } from './Context/AuthContext';
 import Navbar from './assets/components/Navbar/Navbar';
-import Private from './assets/components/Private/Private';
 import Register from './assets/components/Registration/Register';
 import Round from './assets/components/Round/Round';
-
 
 function App() {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
     // Fetch events from Django backend
-    axios.get('http://localhost:8000/api/get-events/')
-      .then(response => {
+    axios
+      .get('http://localhost:8000/api/get-events/')
+      .then((response) => {
         setEvents(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error fetching events:', error);
       });
   }, []);
@@ -34,49 +32,36 @@ function App() {
   return (
     <>
       <Router>
-        
-          <Routes>
-            {/* Define routes for different components */}
-
-            {/* <Route path='/event-form' element={<EventForm/>}/> */}
-            {/* Define routes for other components */}
-
-
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-
-
-              <Route path="/" element={<Home />} />
-              <Route path="/create" element={<Create />} />
-              <Route path="/event" element={<Event />} />
-              <Route path="/schedule" element={<Scheduler />} />
-              {/* Dynamically render routes for each event */}
-              {events.map(event => (
-                <Route
-                  key={event.id}
-                  path={`/${event.name}`}
-                  element={<EventForm eventName={event.name} />}
-                />
-              ))}
-              {events.map(event => (
-                <Route
-                  key={event.id}
-                  path={`/genetic/${event.name}`}
-                  element={<Genetic eventName={event.name} />}
-                />
-
-              ))}
-              <Route path='/round' element={<Round/>}/>
-              <Route path="/enrolled" element={<YourEvents />} />
-              <Route path="/DELETE" element={<Delete />} />
-              
-            </Route>
-
-          </Routes>
-  
+        <Navbar />
+        <Routes>
+          {/* Define routes for different components */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/schedule" element={<Scheduler />} />
+          {events.map((event) => (
+            <Route
+              key={event.id}
+              path={`/${event.name}`}
+              element={<EventForm eventName={event.name} />}
+            />
+          ))}
+          {events.map((event) => (
+            <Route
+              key={event.id}
+              path={`/genetic/${event.name}`}
+              element={<Genetic eventName={event.name} />}
+            />
+          ))}
+          <Route path="/round" element={<Round />} />
+          <Route path="/enrolled" element={<YourEvents />} />
+          <Route path="/DELETE" element={<Delete />} />
+        </Routes>
       </Router>
     </>
-  )
+  );
 }
 
 export default App;
